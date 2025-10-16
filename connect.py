e6d3d6ff676cef2e66be28cf15e5d642c34daba6
Aleksandr <aleksandr.voitushenko@gmail.com>
2025-10-16 16:58:38 +0200

    pylance complience

--- connect.py
+++ connect.py
@@ -4,7 +4,7 @@ from contextlib import contextmanager
 database = './tasks.db'
 
 @contextmanager
-def create_connection(db_file):
+def create_connection(db_file: str):
     conn = sqlite3.connect(db_file)
     yield conn
     conn.rollback()
--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from sqlite3 import Error
+import sqlite3
 from faker import Faker
 from random import randint
 from connect import create_connection, database
@@ -6,8 +6,8 @@ from connect import create_connection, database
 fake = Faker()
 
 
-def gen_users(n: int) -> list[tuple[str,str]]:
-    users = []
+def gen_users(n: int) -> list[tuple[str, str]]:
+    users: list[tuple[str, str]] = []
     for _ in range(n):
         fullname = fake.name()
         email = fake.email()
@@ -15,28 +15,24 @@ def gen_users(n: int) -> list[tuple[str,str]]:
     return users
 
 
-def add_users(conn, users: list[tuple[str,str]]):
+def add_users(conn: sqlite3.Connection,
+               users: list[tuple[str, str]]):
     cursor = conn.cursor()
-    cursor.executemany(
-        'INSERT INTO users (fullname, email) VALUES (?, ?)',
-        users
-    )
+    cursor.executemany("INSERT INTO users (fullname, email) VALUES (?, ?)", users)
     conn.commit()
     cursor.close()
 
 
-def add_status(conn, statuses):
+def add_status(conn: sqlite3.Connection,
+                statuses: list[tuple[str]]):
     cursor = conn.cursor()
-    cursor.executemany(
-        'INSERT INTO status (name) VALUES (?)',
-        statuses
-    )
+    cursor.executemany("INSERT INTO status (name) VALUES (?)", statuses)
     conn.commit()
     cursor.close()
 
 
 def gen_tasks(n: int) -> list[tuple[str, str]]:
-    tasks = []
+    tasks: list[tuple[str, str]] = []
     for _ in range(n):
         title = fake.sentence(nb_words=4)
         description = fake.paragraph(nb_sentences=4)
@@ -44,38 +40,43 @@ def gen_tasks(n: int) -> list[tuple[str, str]]:
     return tasks
 
 
-def add_tasks(conn, tasks: list, users: list, statuses: list):
+def add_tasks(conn: sqlite3.Connection,
+                tasks: list[tuple[str, str]],
+                users: list[tuple[str, str]],
+                statuses: list[tuple[str]]):
 
     cursor = conn.cursor()
 
     for user in users:
-        
+        user_id: int | None = None
         try:
-            cursor.execute('SELECT id FROM users WHERE fullname = ?', (user[0],))
-            user_id = cursor.fetchone()
+            cursor.execute("SELECT id FROM users WHERE fullname = ?", (user[0],))
+            user_id = cursor.fetchone()[0]
+            print(user_id)
 
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
-    
+
         for task in tasks:
             print(task)
-            
+
             status_id = randint(1, len(statuses))
             try:
-                cursor.execute('INSERT INTO tasks (title, description, status_id, user_id) VALUES (?, ?, ?, ?)',
-                                (task[0], task[1], status_id, user_id[0] ))
-            except Error as e:
+                cursor.execute(
+                    "INSERT INTO tasks (title, description, status_id, user_id) VALUES (?, ?, ?, ?)",
+                    (task[0], task[1], status_id, user_id),
+                )
+            except sqlite3.Error as e:
                 print(e)
-    
+
     conn.commit()
     cursor.close()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with create_connection(database) as conn:
         users = gen_users(10)
-        statuses = [('new', ), ('in progress', ), ('completed', )]
+        statuses: list[tuple[str]] = [("new", ), ("in progress", ), ("completed", )]
         tasks = gen_tasks(10)
         add_users(conn, users)
         add_status(conn, statuses)
